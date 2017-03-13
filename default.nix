e323a1f3cd18055e6536795182e7d0bc1a7f6324
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-03-13 08:14:02 -0700

    Add Nix derivation to build project

---
+++ default.nix
@@ -0,0 +1,11 @@
+{ mkDerivation, base, doctest, mtl, stdenv }:
+mkDerivation {
+  pname = "list-transformer";
+  version = "1.0.1";
+  src = ./.;
+  libraryHaskellDepends = [ base mtl ];
+  testHaskellDepends = [ base doctest ];
+  homepage = "https://github.com/Gabriel439/Haskell-List-Transformer-Library";
+  description = "List monad transformer";
+  license = stdenv.lib.licenses.bsd3;
+}
