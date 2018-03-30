c69333b4ecd99f414f0ebfc0d243c912ad5527b1
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-03-30 09:33:05 -0700

    Simplify Nix integration

--- release.nix
+++ release.nix
@@ -1,13 +1,3 @@
-# You can build this repository using Nix by running:
-#
-#     $ nix-build -A list-transformer release.nix
-#
-# You can also open up this repository inside of a Nix shell by running:
-#
-#     $ nix-shell -A list-transformer.env release.nix
-#
-# ... and then Nix will supply the correct Haskell development environment for
-# you
 let
   config = {
     packageOverrides = pkgs: {
@@ -23,5 +13,5 @@ let
     import <nixpkgs> { inherit config; };
 
 in
-  { list-transformer = pkgs.haskellPackages.list-transformer;
+  { inherit (pkgs.haskellPackages) list-transformer;
   }
